157da4478414c924476d58770d93608cfb85887f
Eric Kouassi <39168930+erickouassi@users.noreply.github.com>
2022-10-21 23:49:08 -0400

    Update port

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const http = require("http");
 const AppData = require("./controller");
 const { getReqData } = require("./utils");
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 const server = http.createServer(async (req, res) => {
     // /api/v1 : GET
