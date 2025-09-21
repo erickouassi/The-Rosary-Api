8207d0b5a91d1650dfbbc34329b99ddbeddbe580
Eric Kouassi <39168930+erickouassi@users.noreply.github.com>
2025-09-21 16:24:20 -0400

    Update for 2026 @ 9.21.2025

--- js_active_en_rosary.js
+++ js_active_en_rosary.js
@@ -1,8 +1,8 @@
 //data202X.js
 // For active year
 
-const array1 = require("./years/data_rosary_2024.js");
-const array2 = require("./years/data_rosary_2025.js");
+const array1 = require("./years/data_rosary_2025.js");
+const array2 = require("./years/data_rosary_2026.js");
 
 const newArray = array1.concat(array2);
 
